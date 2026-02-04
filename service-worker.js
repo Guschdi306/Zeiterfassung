1f7d542cf8864c5d426d2a4d175830f716aa25ec
Guschdi306 <warum_talent.0y@icloud.com>
2026-02-05 00:33:34 +0100

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE = "arbeitszeit-v7";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil((async () => {
+    const c = await caches.open(CACHE);
+    await c.addAll(ASSETS);
+    self.skipWaiting();
+  })());
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))));
+    self.clients.claim();
+  })());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith((async () => {
+    const cached = await caches.match(e.request);
+    if (cached) return cached;
+    try {
+      const fresh = await fetch(e.request);
+      return fresh;
+    } catch {
+      return caches.match("./index.html");
+    }
+  })());
+});
